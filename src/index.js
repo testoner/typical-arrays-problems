53bd6d147fd1401a63028481d49d6eab8880b366
testoner <ekgeretoker@gmail.com>
2021-03-10 00:13:23 +0300

    refactor: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (typeof (array) == "undefined") return 0;
+  if (array.length == 0) return 0;
+  let mi = Infinity;
+  for (let i = 0; i < array.length; i++) {
+    if (mi > array[i]) {
+      mi = array[i];
+    }
+  }
+  return mi;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (typeof (array) == "undefined") return 0;
+  if (array.length == 0) return 0;
+  let ma = -Infinity;
+  for (let i = 0; i < array.length; i++) {
+    if (ma < array[i]) {
+      ma = array[i];
+    }
+  }
+  return ma;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (typeof (array) == "undefined") return 0;
+  if (array.length == 0) return 0;
+  let av = 0;
+  let l = array.length;
+  for (let i = 0; i < l; i++) {
+    av += array[i];
+  }
+  av /= l;
+  return av;
 }
